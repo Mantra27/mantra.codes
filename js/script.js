6c1b913e574a11a4009184919251eaa5e6f0d651
Mantra <75286433+Mantra27@users.noreply.github.com>
2021-06-28 11:52:27 +0530

    Update script.js
    Why tho

--- js/script.js
+++ js/script.js
@@ -138,8 +138,7 @@ function update() {
   velocity.tx *= 0.96;
   velocity.ty *= 0.96;
 
-  velocity.x += 0.0001
-  velocity.z += 1
+  velocity.x += 0.009
   velocity.y += ( velocity.ty - velocity.y ) * 0.8;
 
   stars.forEach( ( star ) => {
